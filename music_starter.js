23ff00c17cd5c81dc294467d5b60bfb3e69f4053
pyperjennings <jenninpype@myvuw.ac.nz>
2024-09-09 16:52:19 +1200

    radio

--- music_starter.js
+++ music_starter.js
@@ -7,12 +7,51 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER)
   textSize(24);
   let bassMap = map(bass, 0, 100, 0, 1);
+//radio base
+stroke(101, 202, 252);
+// fill(0);
+rect(250, 350, 300, 170);
+//radio speakers 
+stroke(255, 36, 251);
+// fill(255, 36, 251);
+circle(160, 350, 100, 100)
+circle(340, 350, 100, 100)
+stroke(101, 202, 252);
+// fill(101, 202, 252);
+circle(160, 350, 75, 100)
+circle(340, 350, 75, 100)
+stroke(194, 35, 247);
+// fill(194, 35, 247);
+circle(160, 350, 30, 100)
+circle(340, 350, 30, 100)
+//radio insert
+stroke(194, 35, 247);
+fill(194, 35, 247);
+rect(250, 380, 75, 50);
+stroke(101, 202, 252);
+// fill(101, 202, 252)
+rect(250, 380, 60, 35);
+stroke(194, 35, 247);
+// fill(194, 35, 247)
+circle(235, 380, 10, 150)
+circle(265, 380, 10, 150)
+//radio details 
+stroke(255, 36, 2517);
+// fill(255, 36, 251);
+circle(120, 420, 10, 150)
+circle(380, 420, 10, 150)
+stroke(255, 36, 251);
+// fill(255, 36, 251);
+circle(120, 280, 10, 150)
+circle(380, 280, 10, 150)
+
+
 volhistory.push(bassMap);
 stroke(255);
 noFill();
 beginShape();
 for (var i = 0; i < volhistory.length; i++) {
-  var y = map(volhistory[i], 0, 1, height / 2 , 0);
+  var y = map(volhistory[i], 0, 1, height / 4 , 0);
   vertex(i, y);
 
   ellipse(i, y, 1, 1);
